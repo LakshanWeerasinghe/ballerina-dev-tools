d7f348b22ed03aee5518930242edfa85fa8beb2a
LakshanWeerasinghe <lak98weerasinghe@gmail.com>
2025-03-04 13:41:28 +0530

    Update service-index.sql

--- service-model-generator/modules/service-model-index-generator/src/main/resources/service-index.sql
+++ service-model-generator/modules/service-model-index-generator/src/main/resources/service-index.sql
@@ -1,10 +1,12 @@
 -- Drop tables if they already exist to prevent conflicts
-DROP TABLE IF EXISTS FunctionConnector;
-DROP TABLE IF EXISTS Parameter;
-DROP TABLE IF EXISTS Function;
-DROP TABLE IF EXISTS Connector;
 DROP TABLE IF EXISTS Package;
+DROP TABLE IF EXISTS AttachToService;
+DROP TABLE IF EXISTS Function;
+DROP TABLE IF EXISTS Parameter;
 DROP TABLE IF EXISTS ParameterMemberType;
+DROP TABLE IF EXISTS Annotation;
+DROP TABLE IF EXISTS AnnotationField;
+DROP TABLE IF EXISTS AnnotationFieldMemberType;
 
 -- Create Package table
 CREATE TABLE Package (
@@ -15,27 +17,27 @@ CREATE TABLE Package (
     keywords TEXT
 );
 
+-- Create AttachToService table
+CREATE TABLE AttachToService (
+    attachment_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    kind TEXT CHECK(kind IN ('SERVICE', 'LISTENER')),
+    package_id INTEGER,
+    FOREIGN KEY (package_id) REFERENCES Package(package_id) ON DELETE CASCADE
+);
+
 -- Create Function table
 CREATE TABLE Function (
     function_id INTEGER PRIMARY KEY AUTOINCREMENT,
     kind TEXT CHECK(kind IN ('FUNCTION', 'CONNECTOR', 'REMOTE', 'RESOURCE')),
     name TEXT NOT NULL,
     description TEXT,
-    package_id INTEGER,
+    attachment_id INTEGER,
     return_type JSON, -- JSON type for return type information
     resource_path TEXT NOT NULL,
     return_error INTEGER CHECK(return_error IN (0, 1)),
     inferred_return_type INTEGER CHECK(return_error IN (0, 1)), -- Whether the return type is inferred 
-    FOREIGN KEY (package_id) REFERENCES Package(package_id) ON DELETE CASCADE
-);
-
--- Create FunctionConnector table to define actions for connectors
-CREATE TABLE FunctionConnector (
-    function_id INTEGER,
-    connector_id INTEGER,
-    PRIMARY KEY (function_id, connector_id),
-    FOREIGN KEY (function_id) REFERENCES Function(function_id) ON DELETE CASCADE,
-    FOREIGN KEY (connector_id) REFERENCES Function(function_id) ON DELETE CASCADE
+    FOREIGN KEY (attachment_id) REFERENCES AttachToService(attachment_id) ON DELETE CASCADE
 );
 
 -- Create Parameter table
@@ -61,4 +63,37 @@ CREATE TABLE ParameterMemberType (
     parameter_id INTEGER,
     package TEXT, -- format of the package is org:name:version
     FOREIGN KEY (parameter_id) REFERENCES Parameter(parameter_id) ON DELETE CASCADE
+);
+
+-- Create Annotation table
+CREATE TABLE Annotation (
+    annotation_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    attachment_points TEXT NOT NULL,
+    package_id INTEGER,
+    FOREIGN KEY (package_id) REFERENCES Package(package_id) ON DELETE CASCADE
+);
+
+-- Create Annotation Attachment table
+CREATE TABLE AnnotationField (
+    annotation_field_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    description TEXT,
+    kind TEXT CHECK(kind IN ('REQUIRED', 'DEFAULTABLE', 'INCLUDED_RECORD','INCLUDED_FIELD', 'INCLUDED_RECORD_REST')),
+    type JSON, -- JSON type for parameter type information
+    default_value TEXT,
+    optional INTEGER CHECK(optional IN (0, 1)),
+    import_statements TEXT,
+    annotation_id INTEGER,
+    FOREIGN KEY (annotation_id) REFERENCES Annotation(annotation_id) ON DELETE CASCADE
+);
+
+-- Create Annotation Field Member Type table
+CREATE TABLE AnnotationFieldMemberType (
+    member_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type JSON, -- JSON type for parameter type information
+    kind TEXT,
+    annotation_field_id INTEGER,
+    package TEXT, -- format of the package is org:name:version
+    FOREIGN KEY (annotation_field_id) REFERENCES AnnotationField(annotation_field_id) ON DELETE CASCADE
 );
